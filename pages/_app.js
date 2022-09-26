c2496b7e1ccd5a409b02d9dff165e6b90502bbb7
Raphael Lüthy <raphael@famluethy.chraphael@famluethy.chraphael@famluethy.ch>
2022-09-26 12:55:34 +0200

    Added HTML Input Validation

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import 'nextra-theme-docs/style.css'
+import Prism from 'prism-react-renderer/prism'
 
 export default function Nextra({ Component, pageProps }) {
+  ;(typeof global !== 'undefined' ? global : window).Prism = Prism
+  require('prismjs/components/prism-markup')
+  require('prismjs/components/prism-powershell')
   return <Component {...pageProps} />
 }
